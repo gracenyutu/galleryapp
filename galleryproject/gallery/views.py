68697e375cb8d54ca1bcfc996567543f175d5a48
Grace Nyutu <graceynyutu@gmail.com>
2022-05-28 17:42:15 +0300

    create home and about templates,connect with the relevant views

--- galleryproject/gallery/views.py
+++ galleryproject/gallery/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Gallery Home</h1>')
+    return render(request, 'gallery/home.html')
 
 def about(request):
-    return HttpResponse('<h1> About </h1>')
+    return render(request, 'gallery/about.html')
