23407d84e97eabfac867de62e20fabb9da616882
Grace Nyutu <graceynyutu@gmail.com>
2022-05-28 15:41:35 +0300

    create django project named galleryproject

--- galleryproject/galleryproject/asgi.py
+++ galleryproject/galleryproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for galleryapp project.
+ASGI config for galleryproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'galleryapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'galleryproject.settings')
 
 application = get_asgi_application()
