bfa0c8bfc4c969de42b078cb4128cabb97562274
Grace Nyutu <graceynyutu@gmail.com>
2022-05-29 09:50:17 +0300

    Create Image model with image,description, location foreign key, category foreign key properties

--- galleryproject/gallery/models.py
+++ galleryproject/gallery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
