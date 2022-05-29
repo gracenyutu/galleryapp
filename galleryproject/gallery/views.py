0f744e0482b46d34a52e6d6422f67a3e1c3f526e
Grace Nyutu <graceynyutu@gmail.com>
2022-05-29 19:56:52 +0300

    create the add-photo functionality

--- galleryproject/gallery/views.py
+++ galleryproject/gallery/views.py
@@ -13,7 +13,15 @@ def about(request):
 
 def viewPhoto(request, pk):
     image = Image.objects.get(id=pk)
-    return render(request, 'gallery/viewphoto.html', {'title': 'ViewPhoto'}, {'image': image})
+    return render(request, 'gallery/viewphoto.html',{'image': image})
 
 def addPhoto(request):
-    return render(request, 'gallery/addphoto.html', {'title': 'AddPhoto'})
+    categories = Category.objects.all()
+    location = Location.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+    context = {'categories': categories, 'location': location}
+    return render(request, 'gallery/addphoto.html', context)
