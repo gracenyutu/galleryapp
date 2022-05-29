93a75f836560b1532b4c938d8db7f42f85fef993
Grace Nyutu <graceynyutu@gmail.com>
2022-05-29 12:46:37 +0300

    configure static files

--- galleryproject/galleryproject/settings.py
+++ galleryproject/galleryproject/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
