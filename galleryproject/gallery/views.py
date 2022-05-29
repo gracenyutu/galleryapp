0b0209ef65b7634d617d303784531f750ca06d19
Grace Nyutu <graceynyutu@gmail.com>
2022-05-29 15:07:59 +0300

    update the view image functionality

--- galleryproject/gallery/views.py
+++ galleryproject/gallery/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from .models import Category, Location, Image
 
 # Create your views here.
 def home(request):
-    return render(request, 'gallery/home.html')
+    categories = Category.objects.all()
+    images = Image.objects.all()
+    context = {'categories': categories, 'images': images}
+    return render(request, 'gallery/home.html', context)
 
 def about(request):
     return render(request, 'gallery/about.html', {'title': 'About'})
 
 def viewPhoto(request, pk):
-    return render(request, 'gallery/viewphoto.html', {'title': 'ViewPhoto'})
+    image = Image.objects.get(id=pk)
+    return render(request, 'gallery/viewphoto.html', {'title': 'ViewPhoto'}, {'image': image})
 
 def addPhoto(request):
     return render(request, 'gallery/addphoto.html', {'title': 'AddPhoto'})
