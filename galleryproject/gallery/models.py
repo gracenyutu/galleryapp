aef852da91e8e2ea398d58a8fc41ca6590df1bda
Grace Nyutu <graceynyutu@gmail.com>
2022-05-29 11:30:09 +0300

    apply migrations, create database

--- galleryproject/gallery/models.py
+++ galleryproject/gallery/models.py
@@ -13,12 +13,12 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 class Image(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL)
-    image = models.ImageField(null=False, blank=False)
-    description = models.CharField(max_length=500, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    # image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.description
 
 
