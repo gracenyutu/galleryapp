d8b0d51e1da7c603346da665ed8ceb508162c4ae
Grace Nyutu <graceynyutu@gmail.com>
2022-05-31 12:43:02 +0300

    add modal

--- galleryproject/galleryproject/settings.py
+++ galleryproject/galleryproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['limitless-thicket-56269.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'limitless-thicket-56269.herokuapp.com']
 
 
 # Application definition
